8783de6291b3538868720b0385ea1d58686c74bb
guokey <wszghj@aliyun.com>
2018-05-19 16:25:34 +0800

    mac

--- app/ui/public/ears.js
+++ app/ui/public/ears.js
@@ -6,14 +6,33 @@ import {
     TouchableOpacity,
 } from 'react-native';
 
+import Video from 'react-native-video';
+
 export default class Ears extends Component {
 
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity style={styles.button} activeOpacity={0.5}>
-                    <Text style={{color: 'white'}}>磨耳朵</Text>
-                </TouchableOpacity>
+                <Video
+                    ref={(ref) => {
+                        this.video = ref
+                    }}
+                    //来自本地的MP4视频
+                    source={require('../../video/jmc.mp4')}
+                    //1.0表示默认速率
+                    rate={1.0}
+                    //图片等比例缩放
+                    resizeMode='contain'
+                    //不重复播放
+                    repeat={false}
+                    //默认音量
+                    volume={1.0}
+                    //样式
+                    style={styles.backgroundVideo}/>
+
+                {/*<TouchableOpacity style={styles.button} activeOpacity={0.5}>*/}
+                    {/*<Text style={{color: 'white'}}>磨耳朵</Text>*/}
+                {/*</TouchableOpacity>*/}
             </View>
         );
     }
@@ -33,5 +52,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#4398ff',
+    },
+    backgroundVideo:{
+        width: 400,
+        height: 400,
     }
 });
