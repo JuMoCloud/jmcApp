a2e5e7c803ba6d7d1b52495f9dd4e2e195ec55a5
guohongjian <guo_hj@suixingpay.com>
2018-05-22 09:23:38 +0800

    win10

--- app/ui/public/clock.js
+++ app/ui/public/clock.js
@@ -3,80 +3,35 @@ import {
     View,
     Text,
     StyleSheet,
-    TouchableOpacity
+    TouchableOpacity,
 } from 'react-native';
-import {RNCamera} from 'react-native-camera';
 
 export default class Clock extends Component {
 
     render() {
         return (
             <View style={styles.container}>
-                <RNCamera
-                    ref={ref => {
-                        this.camera = ref;
-                    }}
-                    style={styles.preview}
-
-                    autoFocus = {RNCamera.Constants.AutoFocus.on}
-
-                    captureAudio = {true}
-
-                    type={RNCamera.Constants.Type.front}
-
-                    flashMode={RNCamera.Constants.FlashMode.torch}
-
-                    permissionDialogTitle={'允许使用相机?'}
-                    permissionDialogMessage={'我们需要您允许使用相机的摄像头功能。'}
-                />
-                <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
-                    <TouchableOpacity
-                        onPress={this.takePicture.bind(this)}
-                        style={styles.capture}
-                    >
-                        <Text style={{fontSize: 14}}> 按钮 </Text>
-                    </TouchableOpacity>
-                </View>
+                <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+                    <Text style={{color: 'white'}}>打卡</Text>
+                </TouchableOpacity>
             </View>
         );
     }
-
-    takePicture = async function () {
-        if (this.camera) {
-            const options = {quality: 0.5, base64: true};
-            const data = await this.camera.takePictureAsync(options)
-            console.log(data.uri);
-        }
-    };
 }
 
 const styles = StyleSheet.create({
-
     container: {
         flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'black'
-    },
-    preview: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    capture: {
-        flex: 0,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        padding: 15,
-        paddingHorizontal: 20,
-        alignSelf: 'center',
-        margin: 20
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
     button: {
-        flex: 0,
-        backgroundColor: '#fff',
+        width: 120,
+        height: 45,
         borderRadius: 5,
-        color: '#000',
-        padding: 10,
-        margin: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#4398ff',
     }
 });
